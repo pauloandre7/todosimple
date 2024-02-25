28df1252c3469e46ccad9c383a66e2c7e77a82e1
Paulo André <147359503+pauloandre7@users.noreply.github.com>
2024-02-24 21:45:33 -0300

    Add Task Model

---
+++ src/main/java/com/pauloandre7/todosimple/models/Task.java
@@ -0,0 +1,99 @@
+package com.pauloandre7.todosimple.models;
+
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = Task.TABLE_NAME)
+public class Task {
+    public static final String TABLE_NAME = "task";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", unique = true)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false, updatable = false)
+    private User user;
+
+    @Column(name = "description", length = 255, nullable = false)
+    @NotNull
+    @NotEmpty
+    @Size(min = 1, max=255)
+    private String description;
+
+    public Task(){
+
+    }
+
+    public Task(Long id, User user, String description){
+        this.id = id;
+        this.user = user;
+        this.description = description;
+    }
+
+    public Long getId(){
+        return id;
+    }
+    public User getUser(){
+        return user;
+    }
+    public String getDescription(){
+        return description;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+    public void setUser(User user){
+        this.user = user;
+    }
+    public void setDescription(String description){
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object obj){
+        if (obj == this) 
+            return true;
+
+        if (obj == null)
+            return false;
+
+        if (!(obj instanceof Task)) 
+            return false;
+
+        Task other = (Task) obj;
+
+        if (this.id == null){
+            if (other.id != null)
+                return false;
+            else if (!this.id.equals(other.id))
+                return false;
+        }
+
+        return Objects.equals(this.id, other.id) 
+            && Objects.equals(this.user, other.user)
+            && Objects.equals(this.description, other.description);
+    }
+
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime + result + ((this.id == null) ? 0 : this.id.hashCode());
+
+        return result;
+    }
+}
