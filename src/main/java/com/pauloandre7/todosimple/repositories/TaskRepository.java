00fb0ba46a89a0c40ee466cf1eed1204d41c4e5a
Paulo André <147359503+pauloandre7@users.noreply.github.com>
2024-02-25 22:43:44 -0300

    refactor: Unused Imports commented

--- src/main/java/com/pauloandre7/todosimple/repositories/TaskRepository.java
+++ src/main/java/com/pauloandre7/todosimple/repositories/TaskRepository.java
@@ -1,8 +1,8 @@
 package com.pauloandre7.todosimple.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+// import org.springframework.data.jpa.repository.Query;
+// import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
