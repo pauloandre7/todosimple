e4f6ea4e67fe5fa24dd2587077faeaf81e66d402
Paulo André <147359503+pauloandre7@users.noreply.github.com>
2024-02-25 23:22:01 -0300

    Create TaskService

---
+++ src/main/java/com/pauloandre7/todosimple/services/TaskService.java
@@ -0,0 +1,56 @@
+package com.pauloandre7.todosimple.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.pauloandre7.todosimple.models.Task;
+import com.pauloandre7.todosimple.models.User;
+import com.pauloandre7.todosimple.repositories.TaskRepository;
+
+@Service
+public class TaskService {
+    
+    @Autowired
+    private TaskRepository taskRepo;
+
+    @Autowired
+    private UserService userService;
+    
+    public Task findById(Long id){
+        Optional<Task> task = this.taskRepo.findById(id);
+
+        return task.orElseThrow(() -> new RuntimeException("Tarefa não encontrada!"));
+    }
+
+    @Transactional
+    public Task create(Task obj){
+        User user = this.userService.findById(obj.getUser().getId());
+
+        obj.setId(null);
+        obj.setUser(user);
+        obj = this.taskRepo.save(obj);
+
+        return obj;
+    }
+
+    @Transactional
+    public Task update(Task obj){
+        Task newObj = findById(obj.getId());
+        newObj.setDescription(obj.getDescription());
+
+        return this.taskRepo.save(newObj);
+    }
+
+    public void delete(Long id){
+        findById(id);
+
+        try{
+            this.taskRepo.deleteById(id);
+        } catch(Exception e){
+            throw new RuntimeException("Não é possível excluir pois há entidades relacionadas!");
+        }
+    }
+}
