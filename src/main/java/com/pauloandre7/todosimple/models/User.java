4e4417f68dda3934e04957db0be4d3beadcbd224
Paulo André <147359503+pauloandre7@users.noreply.github.com>
2024-02-28 20:18:51 -0300

    Add lombok

--- src/main/java/com/pauloandre7/todosimple/models/User.java
+++ src/main/java/com/pauloandre7/todosimple/models/User.java
@@ -10,17 +10,23 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
 
 @Entity
 @Table(name = User.TABLE_NAME)
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class User {
     
     public interface CreateUser {
@@ -51,75 +57,7 @@ public class User {
     private String password;
 
     @OneToMany(mappedBy = "user") // mappedBy deve conter o nome da variável na classe Task que mapeia as associações
+    @JsonProperty(access=Access.WRITE_ONLY)
     private List<Task> tasks = new ArrayList<Task>();
 
-    public User(){
-
-    }
-
-    public User(Long id, String username, String password){
-        this.id = id;
-        this.username = username;
-        this.password = password;
-    }
-
-    public Long getId(){
-        return id;
-    }
-    public String getUsername(){
-        return username;
-    }
-    public String getPassword(){
-        return password;
-    }
-    @JsonIgnore // não inclui o dado no json (otimizacao)
-    public List<Task> getTasks(){
-        return tasks;
-    }
-
-    public void setId(Long id){
-        this.id = id;
-    }
-    public void setUsername(String username){
-        this.username = username;
-    }
-    public void setPassword(String password){
-        this.password = password;
-    }
-    public void setTasks(List<Task> tasks){
-        this.tasks = tasks;
-    }
-
-    @Override
-    public boolean equals(Object obj){
-        if (obj == this) 
-            return true;
-
-        if (obj == null)
-            return false;
-
-        if (!(obj instanceof User)) 
-            return false;
-
-        User user = (User) obj;
-
-        if (this.id == null){
-            if (user.id != null)
-                return false;
-            else if (!this.id.equals(user.id))
-                return false;
-        }
-
-        return Objects.equals(id, user.id) 
-            && Objects.equals(username, user.username)
-            && Objects.equals(password, user.password);
-    }
-
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime + result + ((this.id == null) ? 0 : this.id.hashCode());
-
-        return result;
-    }
 }
